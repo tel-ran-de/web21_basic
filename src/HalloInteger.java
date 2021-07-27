acef892e10deb8cbd40215891d1d49dea89091e7
andron13 <andron13@gmail.com>
2021-07-27 12:10:45 +0200

    table

--- src/HalloInteger.java
+++ src/HalloInteger.java
@@ -1,5 +1,8 @@
 public class HalloInteger {
     public static void main(String[] args) {
-
+        byte a = 126;
+        byte b = 2;
+        byte c = (byte) (a + b);
+        System.out.println(c);
     }
 }
