a836809a66319ce28dde6851f94cf12b00f7e32b
andron13 <andron13@gmail.com>
2021-07-29 12:49:37 +0200

    l4  done

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        //addition(10.5, 4);
+        //addition(10.5, 4).sout
+        System.out.println(addition(10.5, 4));
+    }
+
+    public static double addition(double a, double b) {
+        return a + b;
+    }
+}
