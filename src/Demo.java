f384fd2b09c18afb918bc3f7a6be77571ba58ebe
andron13 <andron13@gmail.com>
2021-07-29 13:08:09 +0200

    l4  done

--- src/Demo.java
+++ src/Demo.java
@@ -3,6 +3,8 @@ public class Demo {
         //addition(10.5, 4);
         //addition(10.5, 4).sout
         System.out.println(addition(10.5, 4));
+//        Выведи на телевизор Нокиа в зале
+//                Покажи мне на вебстранице
     }
 
     public static double addition(double a, double b) {
